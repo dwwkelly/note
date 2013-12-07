a041b353b86d82e62e52e6d14bfa74ec1a01b6fd
Devin Kelly <dwwkelly@gmail.com>
2013-12-07 13:33:05 -0500

    added command line args for server.py

--- server.py
+++ server.py
@@ -7,11 +7,26 @@ import daemon
 import os
 import json
 from web import app
+import argparse
+
+
+def parseArgs():
+
+   parser = argparse.ArgumentParser(description='Note Web App')
+   parser.add_argument('-F', action='store_true', help="Run WebApp in foreground (don't daemonize)", default=False)
+   parser.add_argument('-f', type=str, help="Set config file", default=os.path.expanduser('~/.note.conf'))
+
+   args = parser.parse_args()
+   return args
 
 
 def main():
 
-   with open(os.path.expanduser("~/.note.conf")) as fd:
+   args = parseArgs()
+   foreground = args.F
+   configFile = args.f
+
+   with open(configFile) as fd:
       config = json.loads(fd.read())
 
    try:
@@ -35,10 +50,16 @@ def main():
    except:
       listenIP = "127.0.0.1"
 
-   with daemon.DaemonContext():
+   def startServer():
       http_server = HTTPServer(WSGIContainer(app), ssl_options=serverSSLOptions)
       http_server.listen(port, address=listenIP)
       IOLoop.instance().start()
+
+   if foreground:
+      startServer()
+   else:
+      with daemon.DaemonContext():
+         startServer()
 
 
 if __name__ == '__main__':
