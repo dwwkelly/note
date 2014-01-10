4dffe6b819be10d3ccb0e95e07476e3aed8f3473
Devin Kelly <dwwkelly@gmail.com>
2014-01-09 21:24:15 -0500

    made webapp use jinja2 templates not inline text strings for generating html, need to fix search

--- web.py
+++ web.py
@@ -10,78 +10,17 @@ import re
 import os
 import sys
 from flask import Flask
-from flask import url_for
 from flask import request
 from flask import Response
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
 from functools import wraps
 from n import mongoDB
 
 app = Flask(__name__)
-
+app.debug = True
 db = mongoDB("note")
-
-htmlStart = u"""
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Note</title>
-    <link href="/static/dist/css/bootstrap.css" rel="stylesheet">
-    <link href="/static/note.css" rel="stylesheet">
-    <link href="/static/datepicker/css/datepicker.css" rel="stylesheet">
-    <script src="/static/jquery-1.10.2.js"></script>
-    <script src="/static/dist/js/bootstrap.js"></script>
-    <script src="/static/datepicker/js/bootstrap-datepicker.js"></script>
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="Notes">Note</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="{0}">{0}</a></li>
-            <li class="dropdown">
-               <a href="NewNote" class="dropdown-toggle" data-toggle="dropdown">New<b class="caret"></b></a>
-               <ul class="dropdown-menu">
-                  <li><a href="NewNote">Note</a></li>
-                  <li><a href="NewTodo">Todo</a></li>
-                  <li><a href="NewContact">Contact</a></li>
-               </ul>
-            </li>
-            <li><a href="Search">Search</a></li>
-            <li><a href="Delete">Delete</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-
-      <div class="note-template">
-"""
-htmlEnd = u"""
-      </div>
-
-    </div>
-
-  </body>
-</html>
-"""
+j2_env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True)
 
 
 def check_auth(username, password):
@@ -141,40 +80,26 @@ def requires_auth(f):
    return decorated
 
 
-@app.route(r'/Note/Search', methods=["GET", "POST"])
+@app.route(r'/search', methods=["GET", "POST"])
 @requires_auth
 def search():
+
    if request.method == "GET":
-      s = htmlStart.format("Search")
-      s += u"""<form action="Search" method="POST" enctype="multipart/form-data" class="form-inline" role="form">
-              <div class="form-group">
-                 <label for="Search" class="col-sm-2 control-label"></label>
-                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="term">
-                    <input type="hidden" class="form-control" name="api" value="false">
-                 </div>
-              </div>
-           <br><br>
-              <div class="form-group">
-                 <div class="col-sm-offset-2 col-sm-10">
-                    <button type="Search" class="btn btn-default">Search</button>
-                 </div>
-              </div>
-           </form>
-         """
-      s += htmlEnd
+      pages = {"cmd": {"link": "search", "name": "Search"}, "title": "Search"}
+      t = j2_env.get_template('search.html')
+      s = t.render(p=pages)
    elif request.method == "POST" and request.form["api"] == "false":
+      pages = {"cmd": {"link": "search", "name": "Search Result"}, "title": "Search Result"}
+      t = j2_env.get_template('searchResult.html')
+
       term = request.form["term"]
       results = db.searchForItem(term)
-
-      s = htmlStart.format("Search")
+      resultsHTML = []
       for item in results:
-         ID = item['obj'][u"ID"]
-         item = db.getItem(ID)
-         s += u'<div class="devintron">'
-         s += genHTML(int(ID))
-         s += u'</div>'
-      s += htmlEnd
+         ID = int(item['obj'][u"ID"])
+         resultsHTML.append(genHTML(ID))
+
+      s = t.render(p=pages, searchResults=resultsHTML)
    elif request.method == "POST" and request.form["api"] == "true":
       term = request.form["term"]
       s = json.dumps(db.searchForItem(term))
@@ -185,35 +110,18 @@ def search():
    return s
 
 
-@app.route('/Note/Delete', methods=["GET", "POST"])
+@app.route('/delete', methods=["GET", "POST"])
 @requires_auth
 def Delete():
+   pages = {"cmd": {"link": "delete", "name": "Delete"}, "title": "Delete"}
    if request.method == "GET":
-      s = htmlStart.format("Delete")
-      s += u"""<form action="Delete" method="POST" enctype="multipart/form-data" class="form-inline" role="form">
-              <div class="form-group">
-                 <label for="Delete" class="col-sm-2 control-label"></label>
-                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="ID">
-                    <input type="hidden" class="form-control" name="api" value="false">
-                 </div>
-              </div>
-           <br><br>
-              <div class="form-group">
-                 <div class="col-sm-offset-2 col-sm-10">
-                    <button type="Delete" class="btn btn-default">Delete</button>
-                 </div>
-              </div>
-           </form>
-         """
-      s += htmlEnd
+      t = j2_env.get_template('delete.html')
+      s = t.render(p=pages)
    elif request.method == "POST" and request.form["api"] == "false":
       ID = int(request.form["ID"])
       db.deleteItem(ID)
-
-      s = htmlStart.format("Delete")
-      s += u"Item ID={0} deleted".format(ID)
-      s += htmlEnd
+      t = j2_env.get_template('deleted.html')
+      s = t.render(p=pages, itemID=ID)
    elif request.method == "POST" and request.form["api"] == "true":
       ID = int(request.form["ID"])
       result = db.deleteItem(ID)
@@ -225,80 +133,50 @@ def Delete():
    return s
 
 
-@app.route('/Note', methods=["GET"])
+@app.route('/note', methods=["GET"])
 @requires_auth
 def start():
-   s = htmlStart.format("Notes")
-   s += htmlEnd
-
+   pages = {"cmd": {"link": "note", "name": "Note"}, "title": "Note"}
+   t = j2_env.get_template('note.html')
+   s = t.render(p=pages)
    return s
 
 
-@app.route('/Note/Notes', methods=["GET"])
+@app.route('/notes', methods=["GET"])
 @requires_auth
 def Notes():
-   s = htmlStart.format("Notes")
    fourWeeks = 4.0 * 7.0 * 24.0 * 60.0 * 60.0
    now = time.time()
    fourWeeksAgo = now - fourWeeks
    items = db.getByTime(startTime=fourWeeksAgo, endTime=now)
+   pages = {"cmd": {"link": "search", "name": "Search Result"}, "title": "Search Result"}
+   t = j2_env.get_template('searchResult.html')
 
+   resultsHTML = []
    for ID in items:
-      s += '<div class="devintron">'
-      s += genHTML(int(ID))
-      s += '</div>'
+      tmpHTML = addLinks(genHTML(ID))
+      resultsHTML.append(tmpHTML)
 
-   s += htmlEnd
-
-   s = addLinks(s)
-
+   s = t.render(p=pages, searchResults=resultsHTML)
    return s
 
 
-@app.route('/Note/NewNote', methods=["GET", "POST"])
+@app.route('/newNote', methods=["GET", "POST"])
 @requires_auth
 def NewNote():
-   url_for('static', filename='jquery-1.10.2.js')
-   url_for('static', filename='dist/js/bootstrap.js')
 
-   s = htmlStart.format("NewNote")
+   pages = {"cmd": {"link": "newNote", "name": "New Note"}, "title": "New Note"}
    if request.method == "GET":
-      s += u"""
-           <form action="NewNote" method="POST" enctype="multipart/form-data" class="form-horizontal" role="form">
-              <div class="row">
-                  <div class="col-sm-12">
-                     <div class="form-group">
-                        <label for="Note" class="control-label"></label>
-                        <textarea class="form-control" name="noteText" placeholder="Note" rows="3"></textarea>
-                     </div>
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="col-sm-12">
-                     <div class="form-group">
-                        <label for="Tags" class="control-label"></label>
-                        <input type="text" class="form-control" name="tags" placeholder="Tags">
-                        <input type="hidden" class="form-control" name="api" value="false">
-                     </div>
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="form-group">
-                     <div class="col-sm-12">
-                        <button type="Note" class="btn btn-default">Add Note</button>
-                     </div>
-                  </div>
-              </div>
-           </form>"""
-
+      t = j2_env.get_template('newNote.html')
+      s = t.render(p=pages)
    elif request.method == "POST" and request.form["api"] == "false":
       tags = request.form["tags"]
       noteText = request.form["noteText"]
       db.addItem("notes", {"noteText": noteText, "tags": tags})
-      s += u"Note added<br><br>"
-      s += noteText + u"<br>"
-      s += tags + u"<br>"
-      s + htmlEnd
+
+      t = j2_env.get_template('noteAdded.html')
+      s = t.render(p=pages, noteText=noteText, tags=tags )
+
    elif request.method == "POST" and request.form["api"] == "true":
       noteText = request.form["noteText"]
       tags = request.form["tags"].split(",")
@@ -308,79 +186,13 @@ def NewNote():
    return s
 
 
-@app.route('/Note/NewContact', methods=["GET", "POST"])
+@app.route('/newContact', methods=["GET", "POST"])
 @requires_auth
 def NewContact():
-   url_for('static', filename='jquery-1.10.2.js')
-   url_for('static', filename='dist/js/bootstrap.js')
-
-   s = htmlStart.format("NewContact")
+   pages = {"cmd": {"link": "newContact", "name": "New Contact"}, "title": "New Contact"}
    if request.method == "GET":
-      s += u"""
-           <form action="NewContact" method="POST" enctype="multipart/form-data" class="form-horizontal" role="form">
-           <input type="hidden" class="form-control" name="api" value="false">
-
-              <div class="row">
-                  <div class="col-xs-12 col-sm-4">
-                     <div class="form-group">
-                        <label for="Name" class="control-label"></label>
-                        <input type="text" class="form-control" name="name" placeholder="Name">
-                     </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-4">
-                     <div class="form-group">
-                        <label for="Affiliation" class="control-label"></label>
-                        <input type="text" class="form-control" name="affiliation" placeholder="Affiliation">
-                     </div>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="form-group">
-                        <label for="EMail" class="control-label"></label>
-                        <input type="text" class="form-control" name="email" placeholder="EMail">
-                     </div>
-                  </div>
-              </div>
-
-              <div class="row">
-                  <div class="col-xs-12 col-sm-4">
-                     <div class="form-group">
-                        <label for="Mobile Phone" class="control-label"></label>
-                        <input type="text" class="form-control" name="mobile" placeholder="Mobile Phone">
-                     </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-4">
-                     <div class="form-group">
-                        <label for="Work Phone" class="control-label"></label>
-                        <input type="text" class="form-control" name="work" placeholder="Work Phone">
-                     </div>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="form-group">
-                        <label for="Home Phone" class="control-label"></label>
-                        <input type="text" class="form-control" name="home" placeholder="Home Phone">
-                     </div>
-                  </div>
-              </div>
-
-              <div class="row">
-                  <div class="col-sm-12">
-                     <div class="form-group">
-                        <label for="Address" class="control-label"></label>
-                        <input type="text" class="form-control" name="address" placeholder="Address">
-                     </div>
-                  </div>
-              </div>
-
-
-              <div class="row">
-                  <div class="form-group">
-                     <div class="col-sm-12">
-                        <button type="Contact" class="btn btn-default">Add Contact</button>
-                     </div>
-                  </div>
-              </div>
-           </form>"""
-      s += htmlEnd
+      t = j2_env.get_template('newContact.html')
+      s = t.render(p=pages)
    elif request.method == "POST" and request.form["api"] == "false":
       name = request.form["name"]
       affiliation = request.form["affiliation"]
@@ -398,15 +210,8 @@ def NewContact():
                      "EMAIL": email,
                      "HOME PHONE": home}
       db.addItem("contacts", contactInfo)
-
-      s += u"Note added<br><br>"
-      s += name + u"<br>"
-      s += affiliation + u"<br>"
-      s += email + u"<br>"
-      s += work + u"<br>"
-      s += home + u"<br>"
-      s += mobile + u"<br>"
-      s += address + u"<br>"
+      t = j2_env.get_template('contactAdded.html')
+      s = t.render(p=pages, contact=contactInfo)
    elif request.method == "POST" and request.form["api"] == "true":
       contactText = request.form["contactText"]
       tags = request.form["tags"].split(",")
@@ -415,79 +220,26 @@ def NewContact():
    return s
 
 
-@app.route('/Note/NewTodo', methods=["GET", "POST"])
+@app.route('/newTodo', methods=["GET", "POST"])
 @requires_auth
 def NewTodo():
-   url_for('static', filename='jquery-1.10.2.js')
-   url_for('static', filename='dist/js/bootstrap.js')
-   url_for('static', filename='datepicker/js/bootstrap-datepicker.js')
 
-   s = htmlStart.format("NewTodo")
+   pages = {"cmd": {"link": "newTodo", "name": "New ToDo"}, "title": "New ToDo"}
    if request.method == "GET":
-      s += u"""
-           <form action="NewTodo" method="POST" enctype="multipart/form-data" class="form-horizontal" role="form">
-              <input type="hidden" class="form-control" name="api" value="false">
-              <div class="row">
-                  <div class="col-sm-12">
-                     <div class="form-group">
-                        <label for="Todo" class="control-label"></label>
-                        <textarea class="form-control" name="todoText" placeholder="Todo" rows="3"></textarea>
-                     </div>
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="col-sm-6">
-                     <div class="btn-group" data-toggle="buttons">
-                        <label class="btn btn-primary">
-                           <input type="radio" name="options" value="done">Done
-                        </label>
-                        <label class="btn btn-primary">
-                           <input type="radio" name="options" value="undone">Not Done
-                        </label>
-                     </div>
-                  </div>
-                  <div class="col-sm-6">
-                     <input type="text" class="datepicker" name="date" placeholder="Date">
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="form-group">
-                     <div class="col-sm-12">
-                        <button type="Todo" class="btn btn-default">Add Todo</button>
-                     </div>
-                  </div>
-              </div>
-           </form>
-
-           </div>
-
-           </div>
-
-           <script>
-              $(document).ready(function() {
-                    $('.datepicker').datepicker();
-                    });
-           </script>
-
-         </body>
-         </html>
-         """
+      t = j2_env.get_template('newTodo.html')
+      s = t.render(p=pages)
    elif request.method == "POST" and request.form["api"] == "false":
       todoText = request.form["todoText"]
       done = str(request.form['options'])
       date = request.form['date']
+      done = (done == "done")
 
-      if done == "done":
-         done = True
-      else:
-         done = False
-
-      db.addItem("todos", {"todoText": todoText, "done": done, "date": time.mktime(time.strptime(date, "%m/%d/%Y"))})
-
-      s += todoText + u"<br>"
-      s += str(done) + u"<br>"
-      s += str(date) + u"<br>"
-      s + htmlEnd
+      todoItem = {"todoText": todoText, "done": done, "date": time.mktime(time.strptime(date, "%m/%d/%Y"))}
+      db.addItem("todos", todoItem)
+      todoItem['done'] = str(todoItem['done'])
+      todoItem['date'] = str(todoItem['date'])
+      t = j2_env.get_template('todoAdded.html')
+      s = t.render(p=pages, todo=todoItem)
    elif request.method == "POST" and request.form["api"] == "true":
       todoText = request.form["todoText"]
       tags = request.form["tags"].split(",")
