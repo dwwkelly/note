05bf0e02e0f1aa0d8e74932b79231e1e1c833a8d
Devin Kelly <dwwkelly@gmail.com>
2014-01-19 00:56:28 -0500

    added wsgi wrapper to support subdomains

--- server.py
+++ server.py
@@ -10,6 +10,40 @@ from web import app
 import argparse
 
 
+class ReverseProxied(object):
+   '''Wrap the application in this middleware and configure the
+   front-end server to add these headers, to let you quietly bind
+   this to a URL other than / and to an HTTP scheme that is
+   different than what is used locally.
+
+   In nginx:
+   location /myprefix {
+   proxy_pass http://192.168.0.1:5001;
+   proxy_set_header Host $host;
+   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+   proxy_set_header X-Scheme $scheme;
+   proxy_set_header X-Script-Name /myprefix;
+   }
+
+   :param app: the WSGI application
+   '''
+   def __init__(self, app):
+      self.app = app
+
+   def __call__(self, environ, start_response):
+      script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+      if script_name:
+         environ['SCRIPT_NAME'] = script_name
+         path_info = environ['PATH_INFO']
+         if path_info.startswith(script_name):
+            environ['PATH_INFO'] = path_info[len(script_name):]
+
+      scheme = environ.get('HTTP_X_SCHEME', '')
+      if scheme:
+         environ['wsgi.url_scheme'] = scheme
+      return self.app(environ, start_response)
+
+
 def parseArgs():
 
    parser = argparse.ArgumentParser(description='Note Web App')
@@ -51,15 +85,17 @@ def main():
       listenIP = "127.0.0.1"
 
    def startServer():
-      http_server = HTTPServer(WSGIContainer(app), ssl_options=serverSSLOptions)
+      wsgi_app = ReverseProxied(app.wsgi_app)
+      http_server = HTTPServer(WSGIContainer(wsgi_app), ssl_options=serverSSLOptions)
       http_server.listen(port, address=listenIP)
       IOLoop.instance().start()
 
-   if foreground:
-      startServer()
-   else:
-      with daemon.DaemonContext():
-         startServer()
+   startServer()
+   #if foreground:
+   #   startServer()
+   #else:
+   #   with daemon.DaemonContext():
+   #      startServer()
 
 
 if __name__ == '__main__':
