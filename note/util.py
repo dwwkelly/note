f18187681e42139652476e2015603b9a46386dd0
Devin Kelly <dwwkelly@fastmail.fm>
2014-07-24 23:12:14 -0400

    added util library

---
+++ note/util.py
@@ -0,0 +1,24 @@
+
+itemTypes = ["notes", "todos", "contacts", "places"]
+
+colors = dict()
+colors['reset'] = "\033[0m"         # reset
+colors['hicolor'] = "\033[1m"       # hicolor
+colors['underline'] = "\033[4m"     # underline
+colors['invert'] = "\033[7m"        # invert foreground and background
+colors['foreground black'] = "\033[30m"
+colors['foreground red'] = "\033[31m"
+colors['foreground green'] = "\033[32m"
+colors['foreground yellow'] = "\033[33m"
+colors['foreground blue'] = "\033[34m"
+colors['foreground magenta'] = "\033[35m"
+colors['foreground cyan'] = "\033[36m"
+colors['foreground white'] = "\033[37m"
+colors['background black'] = "\033[40m"
+colors['background red'] = "\033[41m"
+colors['background green'] = "\033[42m"
+colors['background yellow'] = "\033[43m"
+colors['background blue'] = "\033[44m"
+colors['background magenta'] = "\033[45m"
+colors['background cyan'] = "\033[46m"
+colors['background white'] = "\033[47m"
