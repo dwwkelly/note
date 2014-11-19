6894f811429fead9ccb53c7262b80f4b0b67ea49
Devin Kelly <dwwkelly@fastmail.fm>
2014-11-18 22:34:03 -0500

    broke up several classes from original note and spread out across three files

---
+++ note/db_api.py
@@ -0,0 +1,22 @@
+class dbBaseClass:
+   __metaclass__ = ABCMeta
+
+   @abstractmethod
+   def addItem(self, itemType, itemContents):
+      pass
+
+   @abstractmethod
+   def getItem(self, itemID):
+      pass
+
+   @abstractmethod
+   def searchForItem(self, searchInfo):
+      pass
+
+   @abstractmethod
+   def deleteItem(self, itemID):
+      pass
+
+   @abstractmethod
+   def makeBackupFile(self):
+      pass
---
+++ note/sql_driver.py
@@ -0,0 +1,25 @@
+def sqliteDB(dbBaseClass):
+
+   def __init__(self):
+
+      return
+
+   def addItem(self, itemType, itemContents):
+
+      return
+
+   def getItem(self, itemID):
+
+      return
+
+   def searchForItem(self, searchInfo):
+
+      return
+
+   def deleteItem(self, itemID):
+
+      return
+
+   def makeBackupFile(self):
+
+      return
