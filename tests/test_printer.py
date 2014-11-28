c26630780784a2d7a9256ace22932707bd0f66ef
Devin Kelly <dwwkelly@fastmail.fm>
2014-11-27 22:30:00 -0500

    added tests for printer

---
+++ tests/test_printer.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+__author__ = "Devin Kelly"
+
+import unittest
+import note
+import sys
+import json
+from StringIO import StringIO
+from note import colors
+
+
+class NotePrinterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.printer = note.Note_Printer()
+        self.output = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.output
+
+    def tearDown(self):
+        self.output.close()
+        sys.stdout = self.saved_stdout
+
+    def test_printer_search_1(self):
+
+        msg = {"status": "OK",
+               "object": {"received search": "asd",
+                          "results": [{"score": 1.1,
+                                       "obj": {"noteText": "asd",
+                                               "tags": ["123"],
+                                               "ID": 18,
+                                               "timestamps": [1417140705.90]},
+                                       "itemType": "notes"}]},
+               "type": "search"}
+
+        self.printer(json.dumps(msg))
+
+        expected = "{fblue}18 {hicolor}{fred}Thu, Nov 27{reset}: asd\n"
+        expected = expected.format(fblue=colors['foreground blue'],
+                                   hicolor=colors['hicolor'],
+                                   fred=colors['foreground red'],
+                                   reset=colors['reset'])
+
+        self.assertEqual(self.output.getvalue(), expected)
+        self.assertIsInstance(self.printer, note.Note_Printer)
+
+    def test_printer_search_2(self):
+
+        msg = {"status": "OK",
+               "object": {"received search": "asd",
+                          "results": [{"score": 1.1,
+                                       "obj": {"noteText": "asd",
+                                               "tags": ["123"],
+                                               "ID": 18,
+                                               "timestamps": [1417140705.90]},
+                                       "itemType": "notes"},
+                                      {"score": 1.4,
+                                      "obj": {"noteText": "asd 2",
+                                              "tags": ["123"],
+                                              "ID": 1,
+                                              "timestamps": [1417140705.90]},
+                                       "itemType": "notes"}
+                                      ]},
+               "type": "search"}
+
+        self.printer(json.dumps(msg))
+
+        expected_1 = "{fblue}18 {hicolor}{fred}Thu, Nov 27{reset}: asd\n"
+        expected_1 = expected_1.format(fblue=colors['foreground blue'],
+                                       hicolor=colors['hicolor'],
+                                       fred=colors['foreground red'],
+                                       reset=colors['reset'])
+        expected_2 = "{fblue}1 {hicolor}{fred}Thu, Nov 27{reset}: asd 2\n"
+        expected_2 = expected_2.format(fblue=colors['foreground blue'],
+                                       hicolor=colors['hicolor'],
+                                       fred=colors['foreground red'],
+                                       reset=colors['reset'])
+
+        expected = expected_1 + expected_2
+        self.assertEqual(self.output.getvalue(), expected)
+        self.assertIsInstance(self.printer, note.Note_Printer)
+
+    def test_error_1(self):
+
+        msg = {"status": "ERROR",
+               "msg": "test message",
+               "type": "search"}
+
+        self.printer(json.dumps(msg))
+
+        s = "{0}ERROR: {1}{2}\n"
+        s = s.format(colors['foreground red'],
+                     colors['foreground black'],
+                     msg['msg'])
+
+        self.assertEqual(self.output.getvalue(), s)
