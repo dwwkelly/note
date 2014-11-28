50e520dd4a2f1a0c91f1677bd7bdada48d04b2e0
Devin Kelly <dwwkelly@fastmail.fm>
2014-11-27 22:29:42 -0500

    corrected bugs in note_printer.py

--- note/note_printer.py
+++ note/note_printer.py
@@ -1,15 +1,22 @@
+import json
+import time
 from util import colors
+
 
 class Note_Printer(object):
 
-    def __init__(self, msg):
+    def __init__(self):
         """
 
         """
 
-        self.msg_json = msg
+    def __call__(self, msg):
+
+        self.msg = json.loads(msg)
         self.status = self.msg['status']
         self.msg_type = self.msg['type']
+
+        self.Print()
 
     def Print(self):
         """
@@ -26,30 +33,47 @@ class Note_Printer(object):
 
         """
 
-
-        msg_type = self.msg['type']
+        self.msg_type = self.msg['type']
         # FIXME - is there a better way to do this?
         f_name = "print_{0}".format(self.msg_type)
-        f = getattr(f_name)
+        try:
+            f = getattr(self, f_name)
+        except AttributeError:
+            return
+
         f()
 
-    def print_NewNote(self):
+    def print_error(self):
         """
 
         """
 
-        ID = self.msg['object']
-        s = "{red}{ID}\n{black} {msg}"
-        s = s.format(colors['foreground red'])
-
-    def print_ERROR(self):
-        """
-
-        """
-
-        s = "{0}ERROR:{1}{2}"
-        s.format(colors['foreground red'],
-                 colors['foreground black'],
-                 self.msg['msg'])
+        s = "{0}ERROR: {1}{2}"
+        s = s.format(colors['foreground red'],
+                     colors['foreground black'],
+                     self.msg['msg'])
 
         print s
+
+    def print_search(self):
+
+        results = self.msg['object']['results']
+
+        for res in results:
+            note_text = res['obj']['noteText']
+            # tags = res['obj']['tags']
+            ID = res['obj']['ID']
+            timestamps = res['obj']['timestamps']
+            timestamp = time.localtime(max(timestamps))
+            noteDate = time.strftime("%a, %b %d", timestamp)
+
+            s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: {noteText}'
+            s = s.format(fblue=colors['foreground blue'],
+                         ID=ID,
+                         hicolor=colors['hicolor'],
+                         fred=colors['foreground red'],
+                         date=noteDate,
+                         reset=colors['reset'],
+                         noteText=note_text.encode('UTF-8'))
+
+            print s
