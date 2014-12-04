5e87dce7563c568d2381abcc04175c13385aa033
Devin Kelly <dwwkelly@fastmail.fm>
2014-12-03 21:12:00 -0500

    fix bad spacing

--- note/note_printer.py
+++ note/note_printer.py
@@ -68,81 +68,81 @@ class Note_Printer(object):
 
     def print_note(self, msg):
 
-            note_text = msg['obj']['note']
-            ID = msg['obj']['ID']
-            timestamps = msg['obj']['timestamps']
-            timestamp = time.localtime(max(timestamps))
-            noteDate = time.strftime("%a, %b %d", timestamp)
+        note_text = msg['obj']['note']
+        ID = msg['obj']['ID']
+        timestamps = msg['obj']['timestamps']
+        timestamp = time.localtime(max(timestamps))
+        noteDate = time.strftime("%a, %b %d", timestamp)
 
-            s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: {note}'
-            s = s.format(fblue=colors['foreground blue'],
-                         ID=ID,
-                         hicolor=colors['hicolor'],
-                         fred=colors['foreground red'],
-                         date=noteDate,
-                         reset=colors['reset'],
-                         note=note_text.encode('UTF-8'))
+        s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: {note}'
+        s = s.format(fblue=colors['foreground blue'],
+                     ID=ID,
+                     hicolor=colors['hicolor'],
+                     fred=colors['foreground red'],
+                     date=noteDate,
+                     reset=colors['reset'],
+                     note=note_text.encode('UTF-8'))
 
-            for ii in s.split('\n'):
-                print textwrap.fill(ii, width=80)
+        for ii in s.split('\n'):
+            print textwrap.fill(ii, width=80)
 
     def print_place(self, msg):
 
-            note_text = msg['obj']['note']
-            place_text = msg['obj']['place']
-            address_text = msg['obj']['address']
-            ID = msg['obj']['ID']
-            timestamps = msg['obj']['timestamps']
-            timestamp = time.localtime(max(timestamps))
-            noteDate = time.strftime("%a, %b %d", timestamp)
+        note_text = msg['obj']['note']
+        place_text = msg['obj']['place']
+        address_text = msg['obj']['address']
+        ID = msg['obj']['ID']
+        timestamps = msg['obj']['timestamps']
+        timestamp = time.localtime(max(timestamps))
+        noteDate = time.strftime("%a, %b %d", timestamp)
 
-            s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: ' +\
-                '{place}, {address}\n{note}'
-            s = s.format(fblue=colors['foreground blue'],
-                         ID=ID,
-                         hicolor=colors['hicolor'],
-                         fred=colors['foreground red'],
-                         date=noteDate,
-                         reset=colors['reset'],
-                         note=note_text.encode('UTF-8'),
-                         address=address_text,
-                         place=place_text)
+        s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: ' +\
+            '{place}, {address}\n{note}'
+        s = s.format(fblue=colors['foreground blue'],
+                     ID=ID,
+                     hicolor=colors['hicolor'],
+                     fred=colors['foreground red'],
+                     date=noteDate,
+                     reset=colors['reset'],
+                     note=note_text.encode('UTF-8'),
+                     address=address_text,
+                     place=place_text)
 
-            for ii in s.split('\n'):
-                print textwrap.fill(ii, width=80)
+        for ii in s.split('\n'):
+            print textwrap.fill(ii, width=80)
 
     def print_todo(self, msg):
 
-            todo_text = msg['obj']['todo']
-            done = msg['obj']['done']
-            todo_date = msg['obj']['date']
-            ID = msg['obj']['ID']
-            timestamps = msg['obj']['timestamps']
-            timestamp = time.localtime(max(timestamps))
-            noteDate = time.strftime("%a, %b %d", timestamp)
+        todo_text = msg['obj']['todo']
+        done = msg['obj']['done']
+        todo_date = msg['obj']['date']
+        ID = msg['obj']['ID']
+        timestamps = msg['obj']['timestamps']
+        timestamp = time.localtime(max(timestamps))
+        noteDate = time.strftime("%a, %b %d", timestamp)
 
-            todo_date = datetime.datetime.fromtimestamp(todo_date)
-            todo_date = todo_date.strftime('%Y-%m-%d')
+        todo_date = datetime.datetime.fromtimestamp(todo_date)
+        todo_date = todo_date.strftime('%Y-%m-%d')
 
-            if done.lower() in ['false', 'no']:
-                done = 'Not Done'
-            elif done.lower() in ['true', 'yes']:
-                done = 'Done'
+        if done.lower() in ['false', 'no']:
+            done = 'Not Done'
+        elif done.lower() in ['true', 'yes']:
+            done = 'Done'
 
-            s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: ' +\
-                '{todo_text}\n\n{done} - {todo_date}'
-            s = s.format(fblue=colors['foreground blue'],
-                         ID=ID,
-                         hicolor=colors['hicolor'],
-                         fred=colors['foreground red'],
-                         date=noteDate,
-                         reset=colors['reset'],
-                         todo_text=todo_text.encode('UTF-8'),
-                         todo_date=todo_date,
-                         done=done)
+        s = '{fblue}{ID} {hicolor}{fred}{date}{reset}: ' +\
+            '{todo_text}\n\n{done} - {todo_date}'
+        s = s.format(fblue=colors['foreground blue'],
+                     ID=ID,
+                     hicolor=colors['hicolor'],
+                     fred=colors['foreground red'],
+                     date=noteDate,
+                     reset=colors['reset'],
+                     todo_text=todo_text.encode('UTF-8'),
+                     todo_date=todo_date,
+                     done=done)
 
-            for ii in s.split('\n'):
-                print textwrap.fill(ii, width=80)
+        for ii in s.split('\n'):
+            print textwrap.fill(ii, width=80)
 
     def print_Get(self):
 
